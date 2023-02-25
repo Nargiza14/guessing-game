4a9ec74a5c1ae3a468e1f7ad093ddbc283e352c7
Nargiza14 <ruzieva.aya@icloud.com>
2023-02-25 21:14:33 +0600

    update: change src file

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,26 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.min;
+        this.max;
+    }
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        const number = Math.round((this.max + this.min)/2);
+        return number;
+         
     }
 
     lower() {
-
+        this.max = Math.round((this.max + this.min)/2);
     }
 
     greater() {
-
+        this.min = Math.round((this.max + this.min)/2);
     }
 }
 
